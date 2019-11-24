c6156259500219bf54ccb40cad70b9f242dcca83
Zhen Data <zhenyang.9261@gmail.com>
2019-11-23 21:36:17 -0500

    Added database schema and seeds files

---
+++ seeds.sql
@@ -0,0 +1,14 @@
+USE bamazon_db;
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("The Guardians", "Books", 14.99, 50),
+	   ("The Dancing Girls", "Books", 9.99, 40),
+       ("Jenga", "Toys", 6.99, 30),
+       ("Lego", "Toys", 18.99, 80),
+       ("Coffee mug", "Household", 4.99, 20),
+       ("Cutting board", "Household", 8.99, 60),
+       ("Griddle", "Household", 34.99, 20),
+       ("Aveeno Body Lotion", "Personal Care", 7.99, 90),
+       ("Nexxus Shampoo", "Personal Care", 9.99, 40),
+       ("Wireless Mouse", "Electronics", 10.99, 30);
+
